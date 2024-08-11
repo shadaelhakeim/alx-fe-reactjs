bb86d51f76247f2c8ea6fdf7ded9f9af9bfdaa80
shada elhakeim <shadaelhakeim@gmail.com>
2024-08-11 21:49:41 +0300

    modified files

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,11 +3,11 @@
 // import viteLogo from '/vite.svg'
 
 import './App.css'
-import WelcomeMessage from './WelcomeMessage'
-import Header from "./Header"
-import MainContent from './MainContent'
-import Footer from './Footer'
-import UserProfile from './UserProfile'
+import WelcomeMessage from './components/WelcomeMessage'
+import Header from "./components/Header"
+import MainContent from './components/MainContent'
+import Footer from './components/Footer'
+import UserProfile from './components/UserProfile'
 
 
 function App() {
