bf94483d4740c6da852e83198715ea20cb4fad2d
shada elhakeim <shadaelhakeim@gmail.com>
2024-08-18 22:46:45 +0300

    add new project

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+// src/components/Footer.jsx
+function Footer() {
+  return (
+    <footer style={{ padding: '10px', textAlign: 'center', borderTop: '1px solid #ccc', marginTop: '20px' }}>
+      <p>&copy; 2024 Our Company. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
