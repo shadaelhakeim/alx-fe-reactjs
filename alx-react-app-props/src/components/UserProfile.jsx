9d01c26eec092707825911102c5a51faa85eae03
shada elhakeim <shadaelhakeim@gmail.com>
2024-08-18 22:05:13 +0300

    modified files

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,15 +1,17 @@
 import PropTypes from 'prop-types';
-const UserProfile = (props) => {
-    return (
-<div style={{ border: '1px solid gray', padding: '10px', margin: '10px' }} >
-   <h2 style={{ color: 'blue' }} > {props.name} </h2>
-   <p>Age: <span style={{ fontWeight: 'bold' }} > {props.age}</span></p>
-   <p>Bio: {props.bio} </p>
- </div>
-    );
-    
-};
+import { useContext } from 'react';  // Import React and useContext
+import UserContext from './UserContext';    // Make sure the path to UserContext is correct
 
+function UserProfile() {
+  const userData = useContext(UserContext);
+
+  return (
+    <div>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
+    </div>
+  );
+}
 
 UserProfile.propTypes = {
     name: PropTypes.string.isRequired,
