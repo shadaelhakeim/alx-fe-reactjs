bf23c492c865e67049c2c1667a5cc18c929496c2
shada elhakeim <shadaelhakeim@gmail.com>
2024-08-18 11:26:13 +0300

    add new folders

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -8,7 +8,7 @@ import Header from "./components/Header"
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 import UserProfile from './components/UserProfile'
-
+import Counter from './components/Counter'
 
 
 
@@ -21,7 +21,7 @@ function App() {
       <Header />
      <WelcomeMessage />
       <MainContent />
-
+       < Counter />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
      
         <Footer />
