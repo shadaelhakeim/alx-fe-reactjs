f05f86e8fc9d4b2876ebf1886ebcce9a3955af6d
shada elhakeim <shadaelhakeim@gmail.com>
2024-08-25 22:44:44 +0300

    modefied file

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -3,9 +3,12 @@ import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
 import FavoritesList from './components/FavoritesList';
 import RecommendationsList from './components/RecommendationsList';
+import RecipeDetails from './components/RecipeDetails';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import SearchBar from './components/SearchBar';
 const App = () => {
   return (
+        <Router>
     <div>
       <h1>Recipe Sharing App</h1>
       <AddRecipeForm />
@@ -13,7 +16,12 @@ const App = () => {
       <RecipeList />
       <FavoritesList />
       <RecommendationsList />
-    </div>
+      <Routes>
+          <Route path="/" element={<RecipeList />} /> {/* Display the list of recipes */}
+          <Route path="/recipe/:id" element={<RecipeDetails />} /> {/* Route for recipe details */}
+        </Routes>
+      </div>
+      </Router>
   );
 };
 
