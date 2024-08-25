242c28dffd7fd1572183218dfc5510de67cf594c
shada elhakeim <shadaelhakeim@gmail.com>
2024-08-25 22:31:34 +0300

    modefied file

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,41 +1,56 @@
-// src/recipeStore.js
-import { create } from "zustand";
+import create from "zustand";
 
 const useRecipeStore = create((set) => ({
   recipes: [],
   searchTerm: "",
   filteredRecipes: [],
 
-  // Add new recipe
+  // Action to add a new recipe
   addRecipe: (newRecipe) =>
     set((state) => ({
       recipes: [...state.recipes, newRecipe],
+      filteredRecipes: [...state.recipes, newRecipe].filter((recipe) =>
+        recipe.title.toLowerCase().includes(state.searchTerm.toLowerCase())
+      ),
     })),
 
-  // Update existing recipe
+  // Action to delete a recipe
+  deleteRecipe: (recipeId) =>
+    set((state) => ({
+      recipes: state.recipes.filter((recipe) => recipe.id !== recipeId),
+      filteredRecipes: state.filteredRecipes.filter(
+        (recipe) => recipe.id !== recipeId
+      ),
+    })),
+
+  // Action to update a recipe
   updateRecipe: (updatedRecipe) =>
     set((state) => ({
       recipes: state.recipes.map((recipe) =>
         recipe.id === updatedRecipe.id ? updatedRecipe : recipe
       ),
+      filteredRecipes: state.filteredRecipes.map((recipe) =>
+        recipe.id === updatedRecipe.id ? updatedRecipe : recipe
+      ),
     })),
 
-  // Delete a recipe
-  deleteRecipe: (id) =>
+  // Action to set the entire recipes array
+  setRecipes: (recipes) =>
     set((state) => ({
-      recipes: state.recipes.filter((recipe) => recipe.id !== id),
-    })),
-
-  // Set the search term
-  setSearchTerm: (term) => set({ searchTerm: term }),
-
-  // Filter recipes based on the search term
-  filterRecipes: () =>
-    set((state) => ({
-      filteredRecipes: state.recipes.filter((recipe) =>
+      recipes: recipes,
+      filteredRecipes: recipes.filter((recipe) =>
         recipe.title.toLowerCase().includes(state.searchTerm.toLowerCase())
+      ),
+    })),
+
+  // Action to set the search term and filter recipes
+  setSearchTerm: (term) =>
+    set((state) => ({
+      searchTerm: term,
+      filteredRecipes: state.recipes.filter((recipe) =>
+        recipe.title.toLowerCase().includes(term.toLowerCase())
       ),
     })),
 }));
 
-export { useRecipeStore };
+export default useRecipeStore;
