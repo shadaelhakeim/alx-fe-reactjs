7ead670bee4e7ee3ef924c585efdd47fdd117a55
shada elhakeim <shadaelhakeim@gmail.com>
2024-08-25 22:03:37 +0300

    modefied file

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,30 +1,20 @@
 // src/components/DeleteRecipeButton.jsx
-// src/components/DeleteRecipeButton.jsx
-import { useNavigate } from 'react-router-dom';
 import { useRecipeStore } from '../recipeStore';
 import PropTypes from 'prop-types';
 
 const DeleteRecipeButton = ({ recipeId }) => {
   const deleteRecipe = useRecipeStore((state) => state.deleteRecipe);
-  const navigate = useNavigate();
-
-  const handleDelete = () => {
-    deleteRecipe(recipeId);
-    navigate('/'); // Redirect to the home page after deletion
-  };
 
   return (
-    <button onClick={handleDelete}>
+    <button onClick={() => deleteRecipe(recipeId)}>
       Delete Recipe
     </button>
   );
-
 };
 
 
-  DeleteRecipeButton.propTypes = {
-    recipeId: PropTypes.number.isRequired,
+DeleteRecipeButton.propTypes = {
+  recipeId: PropTypes.number.isRequired,
 };
-  
+
 export default DeleteRecipeButton;
-
