19e4153541551401082a11bce39b84cbda1df419
shada elhakeim <shadaelhakeim@gmail.com>
2024-08-25 22:34:02 +0300

    modefied file

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,19 +1,16 @@
-// src/components/RecipeList.jsx
 
-import { Link } from 'react-router-dom'; // Add this import
-import { useRecipeStore } from '../recipeStore';
+import { useRecipeStore } from './recipeStore';
 
 const RecipeList = () => {
-  const filteredRecipes = useRecipeStore((state) => state.filteredRecipes);
+  // Access the recipes from the Zustand store
+  const recipes = useRecipeStore((state) => state.filteredRecipes.length > 0 ? state.filteredRecipes : state.recipes);
 
   return (
     <div>
-      {filteredRecipes.map((recipe) => (
+      {recipes.map((recipe) => (
         <div key={recipe.id}>
           <h3>{recipe.title}</h3>
           <p>{recipe.description}</p>
-          {/* Add a Link to the recipe details page */}
-          <Link to={`/recipe/${recipe.id}`}>View Details</Link>
         </div>
       ))}
     </div>
