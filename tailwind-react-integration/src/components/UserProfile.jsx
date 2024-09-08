5744a7d9d1ac7efa3954d9da58f0c9242e763de8
shada elhakeim <shadaelhakeim@gmail.com>
2024-09-08 22:25:19 +0300

    modefied file

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,17 +1,14 @@
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 p-4 sm:p-8 max-w-xs sm:max-w-sm mx-auto my-10 sm:my-20 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out">
+    <div className="user-profile bg-gray-100 p-4 sm:p-4 md:p-8 max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg">
       <img
+        className="rounded-full w-24 h-24 md:w-36 md:h-36 mx-auto"
         src="https://via.placeholder.com/150"
         alt="User"
-        className="rounded-full w-24 h-24 sm:w-36 sm:h-36 mx-auto hover:scale-110 transition-transform duration-300 ease-in-out"
       />
-      <h1 className="text-lg sm:text-xl text-blue-800 my-4 text-center hover:text-blue-500 transition-colors duration-300 ease-in-out">
-        John Doe
-      </h1>
-      <p className="text-sm sm:text-base text-gray-600 text-center">
-        Developer at Example Co. Loves to write code and explore new
-        technologies.
+      <h1 className="text-lg md:text-xl text-blue-800 my-4">John Doe</h1>
+      <p className="text-gray-600 text-sm md:text-base">
+        Developer at Example Co. Loves to write code and explore new technologies.
       </p>
     </div>
   );
