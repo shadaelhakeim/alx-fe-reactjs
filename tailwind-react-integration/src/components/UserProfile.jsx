3ddb5628f58a23b91d266da9e76de46480e1dcdb
shada elhakeim <shadaelhakeim@gmail.com>
2024-09-08 22:28:16 +0300

    modefied file

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,7 +2,7 @@ function UserProfile() {
   return (
     <div className="user-profile bg-gray-100 p-4 sm:p-4 md:p-8 max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg">
       <img
-        className="rounded-full w-24 h-24 md:w-36 md:h-36 mx-auto"
+        className="rounded-full w-24 h-24 sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto"
         src="https://via.placeholder.com/150"
         alt="User"
       />
