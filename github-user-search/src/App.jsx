ab91ee93cf882cc3e9bbfc79419d4b6bb6190a70
shada elhakeim <shadaelhakeim@gmail.com>
2024-09-22 18:47:07 +0300

    added files

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="app-container">
+      <h1>GitHub User Search</h1>
+      <input type="text" placeholder="Search for GitHub user" />
+      <div id="search-results"></div>
+    </div>
+  );
+}
+
+export default App;
---
+++ github-user-search/src/services/githubApi.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const BASE_URL = "https://api.github.com/users";
+
+export const getUserProfile = async (username) => {
+  try {
+    const response = await axios.get(`${BASE_URL}/${username}`);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching user profile:", error);
+    return null;
+  }
+};
